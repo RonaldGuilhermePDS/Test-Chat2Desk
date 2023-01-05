8cd726d9a8137d6479f88fb4c8e37dae16ca0eb9
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2023-01-05 15:42:39 -0300

    feat: ensure SignUpController return 400 if passwordConfirmation fails

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -29,6 +29,10 @@ export class SignUpController implements Controller {
         }
       }
 
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+        return badRequest(new InvalidParamError("passwordConfirmation"));
+      }
+
       const isValid = this.emailValitador.isValid(httpRequest.body.email);
 
       if (!isValid) {
