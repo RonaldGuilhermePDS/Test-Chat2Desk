23a2ff4d54e00ce4ca0ba937877c47edd09f5475
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2023-01-05 15:48:53 -0300

    refactor: remove code duplication

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -29,11 +29,13 @@ export class SignUpController implements Controller {
         }
       }
 
-      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+      const { email, password, passwordConfirmation } = httpRequest.body;
+
+      if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError("passwordConfirmation"));
       }
 
-      const isValid = this.emailValitador.isValid(httpRequest.body.email);
+      const isValid = this.emailValitador.isValid(email);
 
       if (!isValid) {
         return badRequest(new InvalidParamError("email"));
