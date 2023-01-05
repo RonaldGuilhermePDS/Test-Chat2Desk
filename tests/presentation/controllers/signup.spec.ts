d2bf45d6b173bc920b425e30f0d0e204cab11df6
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2023-01-05 17:35:01 -0300

    feat: ensure SignUpController return 200 if valid data is provided

--- tests/presentation/controllers/signup.spec.ts
+++ tests/presentation/controllers/signup.spec.ts
@@ -46,7 +46,7 @@ const makeAddAccount = (): AddAccount => {
       const fakeAccount = {
         id: "valid_id",
         name: "valid_name",
-        email: "valid_email",
+        email: "valid_email@mail.com",
         password: "valid_password",
       };
 
@@ -231,5 +231,27 @@ describe("SignUp Controller", () => {
     sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(500);
     expect(httpResponse.body).toEqual(new ServerError());
+  });
+
+  test("Should return 200 if valid data is provided", () => {
+    const { sut } = makeSut();
+
+    const httpRequest = {
+      body: {
+        name: "valid_name",
+        email: "valid_email@mail.com",
+        password: "valid_password",
+        passwordConfirmation: "valid_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse.body).toEqual({
+      id: "valid_id",
+      name: "valid_name",
+      email: "valid_email@mail.com",
+      password: "valid_password",
+    });
   });
 });
