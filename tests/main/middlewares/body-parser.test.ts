71e826e9babd7ff3cd24cc01a913db027026f844
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2023-01-06 13:40:15 -0300

    test: ensure requests parse body as json

---
+++ tests/main/middlewares/body-parser.test.ts
@@ -0,0 +1,14 @@
+import request from "supertest";
+import app from "@/main/config/app";
+
+describe("Body Parser Middleware", () => {
+  test("Should parse body as json", async () => {
+    app.post("/test_body_parser", (req, res) => {
+      res.send(req.body);
+    });
+    await request(app)
+      .post("/test_body_parser")
+      .send({ body: "any_body" })
+      .expect({ body: "any_body" });
+  });
+});
