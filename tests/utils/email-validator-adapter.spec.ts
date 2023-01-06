942a993742ca52ff33757f08942d08777841ce89
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2023-01-05 21:18:25 -0300

    test: ensure EmailValidatorAdapter calls validator with correct email

--- tests/utils/email-validator-adapter.spec.ts
+++ tests/utils/email-validator-adapter.spec.ts
@@ -25,4 +25,14 @@ describe("EmailValidatorAdapter", () => {
 
     expect(isValid).toBe(true);
   });
+
+  test("Should call validator with correct email", () => {
+    const sut = new EmailValidatorAdapter();
+
+    const isEmailSpy = jest.spyOn(validator, "isEmail");
+
+    sut.isValid("any_email@gmail.com");
+
+    expect(isEmailSpy).toHaveBeenCalledWith("any_email@gmail.com");
+  });
 });
