5dac6b9d87c8e7421211f3b572bf4ff81309698e
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2023-01-05 23:38:36 -0300

    feat: ensure Argon2Adapter returns a hash on success

--- src/infra/criptography/argon2-adapter.ts
+++ src/infra/criptography/argon2-adapter.ts
@@ -3,8 +3,8 @@ import { Encrypter } from "@/data/protocols/encrypter";
 
 export class Argon2Adapter implements Encrypter {
   async encrypt(value: string): Promise<string> {
-    await argon2.hash(value);
+    const hash = await argon2.hash(value);
 
-    return "";
+    return hash;
   }
 }
